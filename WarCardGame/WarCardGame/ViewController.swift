235eacb215f2b0636b5c14a094983e7cf254f2e1
Victor-martinelli <vicbomlen@gmail.com>
2019-07-21 22:06:01 +0200

    First user interface for a card game

---
+++ WarCardGame/WarCardGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WarCardGame
+//
+//  Created by Victor Martinelli on 7/21/19.
+//  Copyright © 2019 Victor Martinelli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
