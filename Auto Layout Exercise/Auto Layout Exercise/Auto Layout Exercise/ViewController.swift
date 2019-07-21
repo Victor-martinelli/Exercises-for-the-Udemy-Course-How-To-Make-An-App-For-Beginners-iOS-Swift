4dedcaf8e2efaff195e883f7aab5240b2271abc8
Victor-martinelli <vicbomlen@gmail.com>
2019-07-21 21:17:26 +0200

    Initial commit

---
+++ Auto Layout Exercise/Auto Layout Exercise/Auto Layout Exercise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Auto Layout Exercise
+//
+//  Created by Victor Martinelli on 6/30/19.
+//  Copyright © 2019 Victor Martinelli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
