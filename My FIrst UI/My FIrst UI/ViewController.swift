54341e491ed7d99f8de014d90cbcf5c3b7bd492d
Victor-martinelli <vicbomlen@gmail.com>
2019-07-28 21:39:16 +0200

    Continued the course

---
+++ My FIrst UI/My FIrst UI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  My FIrst UI
+//
+//  Created by Victor Martinelli on 7/28/19.
+//  Copyright © 2019 Victor Martinelli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
